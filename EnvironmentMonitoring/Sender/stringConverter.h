9e1f36de2ba631bf5d5e91dba1eb22a1ef7f41fd
Srivatsav, Gunisetty <srivatsav.gunisetty@philips.com>
2020-09-12 16:24:26 +0530

    Converter module along with test

---
+++ EnvironmentMonitoring/Sender/stringConverter.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include "iConverter.h"
+
+using namespace std;
+
+class stringConverter : public iConverter
+{
+public:
+	virtual string convert(vector<EnvironmentParameter>& v) override {
+		string result;
+		for (unsigned i = 0; i < v.size(); i++) {
+			result += v[i].toString() + "\n";
+		}
+		return result;
+	}
+};
---
+++ EnvironmentMonitoring/SenderTest/testStringConverter.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include "pch.h"
+#include "../Sender/stringConverter.h"
+using namespace std;
+
+TEST(StringConverter, Convert) {
+	vector<EnvironmentParameter> v;
+	v.push_back(EnvironmentParameter("Temperature", 32.199028, 40, 0, 37, 4));
+	v.push_back(EnvironmentParameter("Humidity", 33, 90, 0, 70, 0));
+	string expectedOutput = "Temperature { CurrentValue: 32.199028, ErrorHighLimit: 40.000000, ErrorLowLimit: 0.000000, WarningHighLimit: 37.000000, WarningLowLimit: 4.000000 } \nHumidity { CurrentValue: 33.000000, ErrorHighLimit: 90.000000, ErrorLowLimit: 0.000000, WarningHighLimit: 70.000000, WarningLowLimit: 0.000000 } \n";
+
+	iConverter* ConverterObj = new(nothrow) stringConverter;
+
+	string output = ConverterObj->convert(v);
+
+	ASSERT_EQ(output, expectedOutput);
+}
